fd7ac6b2467af2bdd000e78926b78258c57a2ab5
Blari <Blarior@gmail.com>
2019-10-28 00:49:37 +0300

    fixs

--- 8. Функции высшего порядка/app.js
+++ 8. Функции высшего порядка/app.js
@@ -1,4 +1,5 @@
 const mainArr = ["my", "name", "is", "Trinity"];
+const mainArr2 = [10, 20, 30];
 
 function firstFunc(arr, handler1) {
     let arr2 = [];
@@ -6,14 +7,30 @@ function firstFunc(arr, handler1) {
     for (let i = 0; i < arr.length; i++) {
         arr2.push(handler1(arr[i]));
     }
-
-    arr2.split("");
-    console.log(`New walue: ${arr2}`);
+    console.log(`New value: ${arr2.join("")}`);
 }
 function concanStr(el) {
-    let str = "";
-    str += el;
-    return str;
+    let el2 = "";
+    for (let i = 0; i < el.length; i++) {
+        el2 += i === 0 ? el[i].toUpperCase() : el[i];
+    }
+
+    return el2;
 }
 
 firstFunc(mainArr, concanStr);
+
+function secondFunc(arr, handler) {
+    let arr2 = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        arr2.push(handler(arr[i]));
+    }
+    console.log(`New value: ${arr2.join(", ")}`);
+}
+
+function multiply(el) {
+    return el * 10;
+}
+
+secondFunc(mainArr2, multiply);
